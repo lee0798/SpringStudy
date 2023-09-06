aa2270fe58c0c7632a082d6ca352a35f5f22929f
dohwan <dohwan0123@naver.com>
2023-09-06 21:06:28 +0900

    client API

--- firstlecture/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ firstlecture/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -2,6 +2,8 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -9,5 +11,31 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data","hello!!");
         return "hello";//hello.html 파일
+    }
+    @GetMapping("hello-mvc")
+    public String hellomvc(@RequestParam(value = "name", required = true) String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+    @GetMapping("hello-spring")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name){
+        return "hello " + name;
+    }
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+    static class Hello{
+        private String name;
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
