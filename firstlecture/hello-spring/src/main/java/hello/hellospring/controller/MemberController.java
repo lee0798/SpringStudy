8ba23881c09de4319673a756dba59b6bcdc7bdc5
dohwan <dohwan0123@naver.com>
2023-09-13 21:39:35 +0900

    memberList

--- firstlecture/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ firstlecture/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,14 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -11,5 +17,24 @@ public class MemberController {
     @Autowired //dependency injection(DI) component어노테이션을 이용한 의존관계 설정 방법
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+    @GetMapping("members/new")
+    public String creatForm(){
+        return "members/createMemberForm";
+    }
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+    @GetMapping("/members")
+    public String list(Model model){
+        //com+op+v 타입 반환값 생성하기
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
