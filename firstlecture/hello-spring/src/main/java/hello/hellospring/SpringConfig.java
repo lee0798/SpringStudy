02da454d6cebe2b449865a49010ab141e4ef7b5f
dohwan <dohwan0123@naver.com>
2023-09-16 21:51:18 +0900

    database

--- firstlecture/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ firstlecture/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,24 +1,27 @@
 package hello.hellospring;
-
+import hello.hellospring.repository.JdbcMemberRepository;
+import hello.hellospring.repository.JdbcTemplateMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
-    /**
-     *
-     * 자바코드로 직접 스프링 빈을 생성하는 방법
-     */
+    private final DataSource dataSource;
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
     @Bean
-    public MemberService memberService(){
+    public MemberService memberService() {
         return new MemberService(memberRepository());
     }
     @Bean
-    public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+    public MemberRepository memberRepository() {
+        //return new MemoryMemberRepository();
+        //return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 
 }
