14f7735748ced566103099693f89a9b1bf703dce
dohwan <dohwan0123@naver.com>
2023-09-18 21:07:47 +0900

    AOP

--- firstlecture/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ firstlecture/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,4 +1,5 @@
 package hello.hellospring;
+import hello.hellospring.aop.TimeTraceAop;
 import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
 import jakarta.persistence.EntityManager;
@@ -42,5 +43,6 @@ public class SpringConfig {
 //        //return new JpaMemberRepository(em);
 //
 //    }
+
 
 }
