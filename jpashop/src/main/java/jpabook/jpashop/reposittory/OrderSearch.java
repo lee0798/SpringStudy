0f6b2177602d6775402a43af1dfe01a945839327
dohwan <dohwan0123@naver.com>
2023-11-15 20:04:40 +0900

    order test

---
+++ jpashop/src/main/java/jpabook/jpashop/reposittory/OrderSearch.java
@@ -0,0 +1,11 @@
+package jpabook.jpashop.reposittory;
+
+import jpabook.jpashop.Domain.OrderStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class OrderSearch {
+    private String memberName; //회원 이름
+    private OrderStatus orderStatus; //주문 상태 order , cancel
+}
